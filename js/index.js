9ddb55922de225490f290843713df04f4cc3d470
Nikita <nikfiery@mail.ru>
2020-10-26 21:52:22 +0300

    add js new post

--- js/index.js
+++ js/index.js
@@ -4,3 +4,15 @@ let btnBurger = document.querySelector('header .burger'),
 btnBurger.addEventListener( 'click', ()=>{
     aside.style.display = aside.style.display === 'block' ? 'none' : 'block';
 }); //проверка кнопочки (бургер)
+
+
+let btn = document.querySelector('.aside__btn .aside__btn--push'),
+    posts = document.querySelector('.main__wrapper .main__post'),
+    newPost = document.querySelector('.main__wrapper .new-post')
+
+btn.addEventListener( 'click', ()=>{
+    posts.style.display = posts.style.display === 'flex' ? 'none' : 'flex',
+    newPost.style.display = newPost.style.display === 'flex' ? 'none' : 'flex',
+    posts.style.display = newPost.style.display === 'flex' ? 'none' : 'flex';
+});//проверка кнопочки (новый пост)
+
